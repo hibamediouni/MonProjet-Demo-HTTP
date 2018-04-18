20aaeba1b8677074461f4815e3416fde8c06c39c
Hiba Mediouni <hibamediouniif5@gmail.com>
2018-04-18 11:45:01 +0100

    Creer Observable on service

--- src/app/services/posts-list.service.ts
+++ src/app/services/posts-list.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { IPost } from '../domain/ipost';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
-@Injectable()
+@Injectable()  // obligatoire puisque elle inject service http
 export class PostsListService {
+URL = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private _http: Http) { }
+getAllPosts(): Observable<IPost[]> {
+return this._http.get(this.URL).map((resp: Response) => <IPost []> resp.json()); // il faut subscribe to observable
 
-  constructor() { }
-getAllPosts(): IPost[] {
-  return[
+
+  /* return[
     { id: 100, userId: 100, title: 'title100', body: 'Body 100'},
     { id: 200, userId: 200, title: 'title200', body: 'Body 200'},
     { id: 300, userId: 300, title: 'title100', body: 'Body 300'},
     { id: 400, userId: 400, title: 'title100', body: 'Body 100'},
-  ];
+  ];*/
 }
 }
